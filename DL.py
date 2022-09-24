1da047af34b8a573e15fdf2a3f8e9832be81505b
夔曦 <feihongwang6@gmail.com>
2022-09-24 13:40:25 +0800

    Delete DL.py

--- DL.py
+++
@@ -1,7 +0,0 @@
-fig, axes = plt.subplots(rows=2, cols=2)
-axes[0,0].set(title='Upper Left')
-axes[0,1].set(title='Upper Right')
-axes[1,0].set(title='Lower Left')
-axes[1,1].set(title='Lower Right')
-
-
